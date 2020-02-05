e8514923c06abccc7165cf31844ce891b49d6540
mrhsgw <miri.hsgw@trad.ocn.ne.jp>
2020-02-05 16:04:24 +0900

    new file:   app/Http/routes.php 	modified:   app/Providers/AppServiceProvider.php 	new file:   app/Task.php 	new file:   database/migrations/2020_02_05_044153_create_tasks_table.php 	new file:   resources/views/common/errors.blade.php 	new file:   resources/views/layouts/app.blade.php 	new file:   resources/views/tasks.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
+use App\Task;
+use illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
 });
